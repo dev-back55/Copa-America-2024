583f58e06836a962d6de9b43d5e9068d1d48075e
Horacio Abitu <ahabitu@gmail.com>
2024-06-21 00:41:33 -0300

    update videos seccion

--- app/media/page.jsx
+++ app/media/page.jsx
@@ -5,7 +5,7 @@ export default function Media() {
 
   return (
     <div className="mt-24 p-3 xl:max-w-6xl md:max-w-5xl xl:mx-auto">
-        <div className="flex flex-col h-max relative justify-center gap-3">
+        <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 p-6 bg-blue-900 xl:max-w-6xl md:max-w-5xl lg:mx-auto rounded-lg">
         {listVideos.map((video, index) => (
           <div key={index} className='gap-2'>
           <DisplayVideo titulo={video.titulo} descrip={video.descrip} url={video.url}/>
