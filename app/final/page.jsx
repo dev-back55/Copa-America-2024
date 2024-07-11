320c1c61acfd4adef7e077ea0843108658072d61
Horacio Abitu <ahabitu@gmail.com>
2024-07-11 11:12:41 -0300

    update hero2 final components refactor

--- app/final/page.jsx
+++ app/final/page.jsx
@@ -1,16 +1,11 @@
-//import Image from "next/image";
 import FinalList from "@/components/final";
-import { getFinal } from "@/lib/actions/partidos";
-//import { obtenerDiaDeLaSemana } from "@/lib/utils";
-//import Link from "next/link";
-//import { Button } from "@/components/ui/button";
 
 export const metadata = {
   title: 'Final Copa América 2024',
 }
 
-export default async function Final() {
-  const final = await getFinal();
+export default function Final() {
+  
 
   return (
     <div className="mt-24 p-3 xl:max-w-6xl md:max-w-5xl xl:mx-auto">
